e7ed303e875a9ac32a8fb6e93366c245d0320ce1
Michael Crumm <mike@crumm.net>
2020-01-16 23:00:27 -0800

    Add Chart struct

--- lib/phoenix/live_dashboard/telemetry_live/metric_conversion.ex
+++ lib/phoenix/live_dashboard/telemetry_live/metric_conversion.ex
@@ -1,5 +1,50 @@
+defmodule Phoenix.LiveDashboard.Chart do
+  @moduledoc false
+
+  @enforce_keys [:id, :kind, :metric]
+  defstruct [
+    :id,
+    :kind,
+    :label,
+    :metric
+  ]
+
+  @type t :: %__MODULE__{
+          id: String.t(),
+          kind: atom,
+          label: nil | String.t(),
+          metric: Telemetry.Metrics.t()
+        }
+end
+
 defmodule Phoenix.LiveDashboard.MetricConversion do
   @moduledoc false
+  @spec to_chart(metric :: Telemetry.Metrics.t()) :: Phoenix.LiveDashboard.Chart.t()
+  def to_chart(%Telemetry.Metrics.Distribution{}) do
+    raise ArgumentError, "LiveDashboard does not yet support distribution metrics"
+  end
+
+  def to_chart(%struct{} = metric) do
+    %Phoenix.LiveDashboard.Chart{
+      id: id(metric),
+      kind: kind(struct),
+      label: label(metric),
+      metric: metric
+    }
+  end
+
+  # Returns the DOM ID for the chart on the dashboard.
+  defp id(%struct{} = metric) do
+    [metric.name, metric.tags, kind(struct)]
+    |> Enum.flat_map(&List.wrap/1)
+    |> Enum.join("-")
+  end
+
+  defp kind(Telemetry.Metrics.Counter), do: :counter
+  defp kind(Telemetry.Metrics.Distribution), do: :distribution
+  defp kind(Telemetry.Metrics.LastValue), do: :last_value
+  defp kind(Telemetry.Metrics.Sum), do: :sum
+  defp kind(Telemetry.Metrics.Summary), do: :summary
 
   def label(%_struct{} = metric) do
     metric.name
