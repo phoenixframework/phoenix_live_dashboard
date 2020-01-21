4bc9de19714df1b8205506d89663fb292a848261
Michael Crumm <mike@crumm.net>
2020-01-21 15:29:34 -0800

    render static assets into layout

--- lib/phoenix/live_dashboard/layout_view.ex
+++ lib/phoenix/live_dashboard/layout_view.ex
@@ -5,4 +5,16 @@ defmodule Phoenix.LiveDashboard.LayoutView do
   use Phoenix.View,
     namespace: Phoenix.LiveDashboard,
     root: "lib/phoenix/live_dashboard/templates"
+
+  js_path = Path.join(__DIR__, "../../../priv/static/js/app.js")
+  css_path = Path.join(__DIR__, "../../../priv/static/css/app.css")
+
+  @external_resource js_path
+  @external_resource css_path
+
+  @app_js File.read!(js_path)
+  @app_css File.read!(css_path)
+
+  def render("app.js", _), do: @app_js
+  def render("app.css", _), do: @app_css
 end
