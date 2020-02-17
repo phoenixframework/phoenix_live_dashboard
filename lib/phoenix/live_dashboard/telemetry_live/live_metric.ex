20b96288c46d350c499e48e004b2b16ea1a0b5d7
Michael Crumm <mike@crumm.net>
2020-02-16 20:45:24 -0800

    Refactor LiveMetric to move memory to client
    - LiveMetric temporary_assigns data on mount
    - PhxLiveMetric hook calculates chart values in js objects

--- lib/phoenix/live_dashboard/telemetry_live/live_metric.ex
+++ lib/phoenix/live_dashboard/telemetry_live/live_metric.ex
@@ -2,78 +2,28 @@ defmodule Phoenix.LiveDashboard.LiveMetric do
   # A LiveComponent for rendering a `Telemetry.Metrics` chart on the dashboard.
   @moduledoc false
   use Phoenix.LiveComponent
-  alias Phoenix.LiveDashboard.Chart
 
-  @timeseries ~w(summary distribution)a
-  @gauges ~w(last_value counter sum)a
+  @impl true
+  def mount(socket) do
+    {:ok, assign(socket, data: []), temporary_assigns: [data: []]}
+  end
 
   @impl true
   def render(assigns) do
     ~L"""
-    <div class="phx-dashboard-col">
-      <div id="<%= @chart.id %>-datasets" phx-hook="PhxLiveMetric" style="display:none;"><%= render_datasets(assigns) %></div>
+    <div id="<%= @chart.id %>" class="phx-dashboard-col">
+      <div phx-hook="PhxLiveMetric" id="<%= @chart.id %>--datasets" class="datasets">
+      <%= for %{x: x, y: y, z: z} <- @data do %>
+        <span data-x="<%= x %>" data-y="<%= y %>" data-z="<%= z %>"></span>
+      <% end %>
+      </div>
       <div class="chart" phx-update="ignore">
-        <canvas id="<%= @chart.id %>"
+        <canvas id="<%= @chart.id %>--canvas"
          data-label="<%= @chart.label %>"
-         data-type="<%= chartjs_type(@chart) %>"
+         data-metric="<%= @chart.kind %>"
          data-title="<%= @chart.id %>"></canvas>
       </div>
     </div>
-    """
-  end
-
-  @impl true
-  def update(%{chart: %Chart{} = chart}, socket) do
-    {:ok, assign(socket, chart: chart, datasets: [])}
-  end
-
-  def update(%{data: points}, socket) when is_list(points) do
-    %{chart: chart, datasets: datasets} = socket.assigns
-
-    {:ok,
-     assign(
-       socket,
-       :datasets,
-       Enum.reduce(points, datasets, fn point, datasets ->
-         %{x: label, y: measurement, z: received_at} = point
-
-         # get the dataset value for the given label
-         {_, current_value} = List.keyfind(datasets, label, 0, {label, 0})
-
-         List.keystore(
-           datasets,
-           label,
-           0,
-           {label, next_value(chart.kind, measurement, current_value, received_at)}
-         )
-       end)
-     )}
-  end
-
-  defp next_value(:counter, _, current_value, _), do: current_value + 1
-  defp next_value(:last_value, measurement, _, _), do: measurement
-  defp next_value(:sum, measurement, current_value, _), do: measurement + current_value
-  defp next_value(:summary, measurement, _, received_at), do: {received_at, measurement}
-
-  @doc false
-  def chartjs_type(%Chart{kind: kind}), do: chartjs_type(kind)
-  def chartjs_type(kind) when kind in @timeseries, do: "line"
-  def chartjs_type(_), do: "doughnut"
-
-  @doc false
-  def render_datasets(%{chart: %Chart{kind: kind}} = assigns) when kind in @timeseries do
-    ~L"""
-    <%= for {label, {x, y}} <- @datasets do %>
-      <span class="dataset" data-label="<%= label %>" data-x="<%= x %>" data-y="<%= y %>"></span>
-    <% end %>
-    """
-  end
-
-  def render_datasets(%{chart: %Chart{kind: kind}} = assigns) when kind in @gauges do
-    ~L"""
-    <%= for {label, value} <- @datasets do %>
-      <span class="dataset" data-label="<%= label %>" data-value="<%= value %>"></span>
-    <% end %>
     """
   end
 end
