a34e95af887cd5318e0908e8b9ebc276210372e9
José Valim <jose.valim@dashbit.co>
2020-03-03 13:17:51 +0100

    Install nprogress

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,29 @@
 import css from "../css/app.css"
 import "phoenix_html"
 import { Socket } from "phoenix"
+import NProgress from "nprogress"
 import { LiveSocket } from "phoenix_live_view"
 import PhxChartComponent from "./metrics_live"
 
-const PHX_SOCKET = "/live"
-
-/* The LiveView Hooks for the Phoenix.LiveDashboard components. */
 let Hooks = {
   PhxChartComponent: PhxChartComponent
 }
 
-let socketPath = document.querySelector("html").getAttribute("phx-socket") || PHX_SOCKET
+let socketPath = document.querySelector("html").getAttribute("phx-socket") || "/live"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket(socketPath, Socket, {
   hooks: Hooks,
   params: { _csrf_token: csrfToken }
 })
 
+// Show progress bar on live navigation and form submits
+window.addEventListener("phx:page-loading-start", info => NProgress.start())
+window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+
+// connect if there are any LiveViews on the page
 liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)
+window.liveSocket = liveSocket
