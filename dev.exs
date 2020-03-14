e762a087c7ae3eef5b7414fc65112f9bf12782ac
Michael Crumm <mike@crumm.net>
2020-03-13 19:48:03 -0700

    Add paths to dev router for chart testing

--- dev.exs
+++ dev.exs
@@ -51,6 +51,28 @@ defmodule DemoWeb.Telemetry do
   end
 end
 
+defmodule DemoWeb.PageController do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, :index) do
+    content(conn, """
+    <h2>Phoenix LiveDashboard Dev</h2>
+    <a href="/dashboard" target="_blank">Open Dashboard</a>
+    """)
+  end
+
+  def call(conn, :hello), do: content(conn, "<p>Hello, friend!</p>")
+  def call(conn, :hello_name), do: content(conn, "<p>Hello, #{conn.params["name"]}!</p>")
+
+  defp content(conn, content) do
+    conn
+    |> put_resp_header("content-type", "text/html")
+    |> send_resp(200, "<!doctype html><html><body>#{content}</body></html>")
+  end
+end
+
 defmodule DemoWeb.Router do
   use Phoenix.Router
   import Phoenix.LiveDashboard.Router
@@ -62,6 +84,9 @@ defmodule DemoWeb.Router do
 
   scope "/" do
     pipe_through :browser
+    get "/", DemoWeb.PageController, :index
+    get "/hello", DemoWeb.PageController, :hello
+    get "/hello/:name", DemoWeb.PageController, :hello_name
     live_dashboard("/dashboard", metrics: DemoWeb.Telemetry)
   end
 end
