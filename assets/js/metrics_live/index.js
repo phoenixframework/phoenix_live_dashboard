77c50984c617103bf9a6cb7db02fc40d9c49ea03
Przemyslaw Krowinski <hello@krowinski.com>
2020-04-24 17:06:41 +0200

    Use '==' for null comparison

--- assets/js/metrics_live/index.js
+++ assets/js/metrics_live/index.js
@@ -6,13 +6,7 @@ const SeriesValue = (options) => {
   if (!options.unit) return {}
 
   return {
-    value: (u, v) => {
-      if (v === null || typeof v === 'undefined') {
-        return '--'
-      } else {
-        return v.toFixed(3) + ` ${options.unit}`
-      }
-    }
+    value: (u, v) => v == null ? '--' : v.toFixed(3) + ` ${options.unit}`
   }
 }
 
