c2ac4ea6420c6f03821a316f537fe94398919b18
Kian-Meng Ang <kianmeng@cpan.org>
2021-05-10 21:24:12 +0800

    Add rendering test for card component
    This adds the HTML rendering test for card component and uses double
    quote for all HTML attributes.

--- test/phoenix/live_dashboard/components/card_component_test.exs
+++ test/phoenix/live_dashboard/components/card_component_test.exs
@@ -1,7 +1,30 @@
 defmodule Phoenix.LiveDashboard.CardComponentTest do
   use ExUnit.Case, async: true
+  import Phoenix.LiveViewTest
 
   alias Phoenix.LiveDashboard.CardComponent
+  @endpoint Phoenix.LiveDashboardTest.Endpoint
+
+  describe "rendering" do
+    test "card component" do
+      result =
+        render_component(CardComponent,
+          value: "test-value",
+          title: "test-title",
+          hint: "test-hint",
+          class: ["test-class-1", "test-class-2"],
+          inner_title: "test-inner-title",
+          inner_hint: "test-inner-hint"
+        )
+
+      assert result =~ ~r|<h5 class=\"card-title\">[\r\n\s]*test-title[\r\n\s]*|
+      assert result =~ ~S|<div class="hint-text">test-hint</div>|
+      assert result =~ ~S|<div class="banner-card mt-auto test-class-1 test-class-2">|
+      assert result =~ ~r|<h6 class=\"banner-card-title\">[\r\n\s]*test-inner-title[\r\n\s]*|
+      assert result =~ ~S|<div class="hint-text">test-inner-hint</div>|
+      assert result =~ ~S|<div class="banner-card-value">test-value</div>|
+    end
+  end
 
   describe "normalize_params/1" do
     test "validates required params" do
