7e73331eab1ed53c5e9de765750227c494da227c
Kian Meng Ang <kianmeng@cpan.org>
2021-05-24 22:14:12 +0800

    Test rendering for row component (#277)
    This PR adds test cases for rendering a row component.

--- test/phoenix/live_dashboard/components/row_component_test.exs
+++ test/phoenix/live_dashboard/components/row_component_test.exs
@@ -1,7 +1,44 @@
 defmodule Phoenix.LiveDashboard.RowComponentTest do
   use ExUnit.Case, async: true
+  import Phoenix.LiveViewTest
 
   alias Phoenix.LiveDashboard.RowComponent
+  @endpoint Phoenix.LiveDashboardTest.Endpoint
+
+  defmodule SimpleComponent do
+    use Phoenix.LiveComponent
+
+    def render(assigns) do
+      ~L"""
+      <div><%= @text %></div>
+      """
+    end
+  end
+
+  describe "rendering" do
+    test "one row in row component" do
+      result =
+        render_component(RowComponent,
+          components: [{SimpleComponent, %{text: "test-text"}}]
+        )
+
+      assert result =~ ~r|<div class=\"row\">[\r\n\s]*<div>test-text<\/div>[\r\n\s]*<\/div>|
+    end
+
+    test "three rows in row component" do
+      result =
+        render_component(RowComponent,
+          components: [
+            {SimpleComponent, %{text: "test-text-1"}},
+            {SimpleComponent, %{text: "test-text-2"}},
+            {SimpleComponent, %{text: "test-text-3"}}
+          ]
+        )
+
+      assert result =~
+               ~r|<div class=\"row\">[\r\n\s]*<div>test-text-1<\/div>[\r\n\s]*<div>test-text-2<\/div>[\r\n\s]*<div>test-text-3<\/div>[\r\n\s]*<\/div>|
+    end
+  end
 
   describe "normalize_params/1" do
     test "validates required params" do
