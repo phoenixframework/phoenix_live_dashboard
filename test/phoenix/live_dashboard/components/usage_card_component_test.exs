c7458cb69573715e7ae4c50343573b0095f8d4f9
Kian Meng Ang <kianmeng@cpan.org>
2021-05-28 23:34:42 +0800

    Test rendering for usage card component (#284)
    This PR adds test case for rendering in usage card component.

--- test/phoenix/live_dashboard/components/usage_card_component_test.exs
+++ test/phoenix/live_dashboard/components/usage_card_component_test.exs
@@ -1,7 +1,44 @@
 defmodule Phoenix.LiveDashboard.UsageCardComponentTest do
   use ExUnit.Case, async: true
 
+  import Phoenix.LiveViewTest
+
   alias Phoenix.LiveDashboard.UsageCardComponent
+  @endpoint Phoenix.LiveDashboardTest.Endpoint
+
+  describe "rendering" do
+    test "usage card component" do
+      result =
+        render_component(UsageCardComponent,
+          usages: [
+            %{
+              current: 10,
+              limit: 150,
+              dom_sub_id: "test-dom-sub-id",
+              title: "test-usage-title",
+              hint: "test-usage-hint",
+              percent: 13
+            }
+          ],
+          dom_id: "test-dom-id",
+          title: "test-title",
+          hint: "test-hint",
+          csp_nonces: %{img: "img_nonce", style: "style_nonce", script: "script_nonce"}
+        )
+
+      assert result =~ ~r|<h5 class=\"card-title\">[\r\n\s]*test-title[\r\n\s]*|
+      assert result =~ ~S|<div class="hint-text">test-hint</div>|
+
+      assert result =~ ~r|<div>[\r\n\s]*test-usage-title[\r\n\s]*|
+      assert result =~ ~S|<div class="hint-text">test-usage-hint</div>|
+
+      assert result =~ ~r|<small class=\"text-muted pr-2\">[\r\n\s]*10 / 150[\r\n\s]*<\/small>|
+      assert result =~ ~r|<strong>[\r\n\s]*13%[\r\n\s]*</strong>|
+
+      assert result =~
+               ~r|<style nonce=\"style_nonce\">#test-dom-id-test-dom-sub-id-progress{width:13%}<\/style>|
+    end
+  end
 
   describe "normalize_params/1" do
     test "validates required params" do
