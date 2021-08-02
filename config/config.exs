7be1f3616538de3c19696945f9a534be1bd5f577
Max Veytsman <maxim@ontoillogical.com>
2021-08-02 10:49:31 -0400

    Update config/config.exs
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,7 @@ config :logger, level: :warn
 config :logger, :console, format: "[$level] $message\n"
 
 config :esbuild,
-  version: "0.12.15",
+  version: "0.12.17",
   default: [
     args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
