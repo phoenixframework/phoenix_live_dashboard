d13b5b4abd9d7e56a8d767d2dcb02b82152b2c64
Max Veytsman <maxim@ontoillogical.com>
2021-08-11 13:06:26 -0400

    Only config esbuild in dev, and add sourcemap

--- config/config.exs
+++ config/config.exs
@@ -6,10 +6,12 @@ config :phoenix, :stacktrace_depth, 20
 config :logger, level: :warn
 config :logger, :console, format: "[$level] $message\n"
 
-config :esbuild,
-  version: "0.12.17",
-  default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
-    cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
+if Mix.env() == :dev do
+  config :esbuild,
+    version: "0.12.17",
+    default: [
+      args: ~w(js/app.js --bundle --sourcemap --target=es2016 --outdir=../priv/static/assets),
+      cd: Path.expand("../assets", __DIR__),
+      env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    ]
+end
