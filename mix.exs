3737e6c5421c10f5d3be92fcb5f6b9744275c5a3
Dave Lucia <davelucianyc@gmail.com>
2022-08-13 16:11:05 -0400

    Upgrade ecto_mysql_extras to latest (#374)

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Phoenix.LiveDashboard.MixProject do
       {:phoenix_live_view, "~> 0.17.7", phoenix_live_view_opts()},
       {:telemetry_metrics, "~> 0.6.0"},
       {:ecto_psql_extras, "~> 0.7", optional: true},
-      {:ecto_mysql_extras, "~> 0.3", optional: true},
+      {:ecto_mysql_extras, "~> 0.5", optional: true},
       {:ecto, "~> 3.6.2 or ~> 3.7", optional: true},
 
       # Dev and test
