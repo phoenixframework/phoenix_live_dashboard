e708b28c3e450c717dff2ee28c86c483da756ac0
Michael Crumm <mike@crumm.net>
2022-09-18 13:40:45 -0700

    Add esbuild mix package

--- mix.exs
+++ mix.exs
@@ -33,8 +33,9 @@ defmodule Phoenix.LiveDashboard.MixProject do
 
   defp aliases do
     [
-      setup: ["deps.get", "cmd npm install --prefix assets"],
-      dev: "run --no-halt dev.exs"
+      setup: ["deps.get", "cmd --cd assets npm install"],
+      dev: "run --no-halt dev.exs",
+      "assets.deploy": ["esbuild default --minify"]
     ]
   end
 
@@ -56,7 +57,8 @@ defmodule Phoenix.LiveDashboard.MixProject do
       {:jason, "~> 1.0", only: [:dev, :test, :docs]},
       {:floki, "~> 0.27.0", only: :test},
       {:stream_data, "~> 0.1", only: :test},
-      {:ex_doc, "~> 0.21", only: :docs}
+      {:ex_doc, "~> 0.21", only: :docs},
+      {:esbuild, "~> 0.5", only: :dev}
     ]
   end
 
