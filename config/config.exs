bee8a8c32f1d7766312ac381b65af5a88b739789
Michael Crumm <mike@crumm.net>
2022-09-18 14:26:50 -0700

    Configure esbuild and assets alias

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,11 @@ config :phoenix, :stacktrace_depth, 20
 
 config :logger, level: :warn
 config :logger, :console, format: "[$level] $message\n"
+
+config :esbuild,
+  version: "0.14.41",
+  default: [
+    args: ~w(js/app.js --bundle --target=es2020 --outdir=../priv/static/assets),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
