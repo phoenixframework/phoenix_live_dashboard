7b49e8374c35589a76d4126da920bd3a2f323bbb
Chris McCord <chris@chrismccord.com>
2022-10-26 15:29:03 -0400

    Bump locals until phoenix ships them

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,12 @@
 # Used by "mix format"
+locals_without_parens = [
+  embed_templates: 1,
+  embed_templates: 2,
+]
+
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
