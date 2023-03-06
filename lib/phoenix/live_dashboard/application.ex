8354b49ffc159a6a5bb8ab454389ad0322ca5d60
Alex Castaño <alexcastanodev@gmail.com>
2023-03-06 23:10:51 +0100

    Fix compilation warning

--- lib/phoenix/live_dashboard/application.ex
+++ lib/phoenix/live_dashboard/application.ex
@@ -6,21 +6,27 @@ defmodule Phoenix.LiveDashboard.Application do
     # Preload it as we check if it is available on remote nodes
     Code.ensure_loaded(Phoenix.LiveDashboard.SystemInfo)
 
-    if function_exported?(Logger, :default_formatter, 0) do
-      :ok =
-        :logger.add_handler(
-          Phoenix.LiveDashboard.LoggerPubSubBackend,
-          Phoenix.LiveDashboard.LoggerPubSubBackend,
-          %{formatter: Logger.default_formatter(colors: [enabled: false])}
-        )
-    else
-      Logger.add_backend(Phoenix.LiveDashboard.LoggerPubSubBackend)
-    end
+    add_logger_backend()
 
     children = [
       {DynamicSupervisor, name: Phoenix.LiveDashboard.DynamicSupervisor, strategy: :one_for_one}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  if function_exported?(Logger, :default_formatter, 0) do
+    defp add_logger_backend() do
+      :ok =
+        :logger.add_handler(
+          Phoenix.LiveDashboard.LoggerPubSubBackend,
+          Phoenix.LiveDashboard.LoggerPubSubBackend,
+          %{formatter: Logger.default_formatter(colors: [enabled: false])}
+        )
+    end
+  else
+    defp add_logger_backend() do
+      Logger.add_backend(Phoenix.LiveDashboard.LoggerPubSubBackend)
+    end
   end
 end
