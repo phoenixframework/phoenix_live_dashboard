78d772d590bf8c379d7cca435a3ee575bdc0f25f
Leandro Pereira <leandro@leandro.io>
2024-03-01 05:34:56 -0500

    Add immutable directive to cache-control header (#440)
    Same as https://github.com/elixir-plug/plug/pull/1205
    
    That's a good practice since the assets are hashed

--- lib/phoenix/live_dashboard/controllers/assets.ex
+++ lib/phoenix/live_dashboard/controllers/assets.ex
@@ -34,7 +34,7 @@ defmodule Phoenix.LiveDashboard.Assets do
 
     conn
     |> put_resp_header("content-type", content_type)
-    |> put_resp_header("cache-control", "public, max-age=31536000")
+    |> put_resp_header("cache-control", "public, max-age=31536000, immutable")
     |> put_private(:plug_skip_csrf_protection, true)
     |> send_resp(200, contents)
     |> halt()
