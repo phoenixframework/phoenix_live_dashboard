fc01841f1923451ea8a88550e6e555ca495c542e
Fredrik Teschke <f@ftes.de>
2026-09-10 16:17:55 +0200

    Group remote link regression with existing app tree tests

--- test/phoenix/live_dashboard/system_info_test.exs
+++ test/phoenix/live_dashboard/system_info_test.exs
@@ -291,6 +291,35 @@ defmodule Phoenix.LiveDashboard.SystemInfoTest do
               [{{:ancestor, _, []}, [{{:supervisor, _, :kernel_sup}, [_ | _]}]}]} =
                SystemInfo.fetch_app_tree(node(), :kernel)
     end
+
+    test "returns the tree when a process has a remote link" do
+      peer = start_peer()
+      remote_peer = start_peer()
+      remote_pid = :peer.call(remote_peer, :erlang, :spawn, [:timer, :sleep, [:infinity]])
+
+      child = %{id: :linked_worker, start: {Agent, :start_link, [:erlang, :link, [remote_pid]]}}
+      {:ok, worker} = :peer.call(peer, Supervisor, :start_child, [:kernel_safe_sup, child])
+
+      assert {:links, links} = :peer.call(peer, Process, :info, [worker, :links])
+      assert remote_pid in links
+
+      assert {{:master, _, []},
+              [{{:ancestor, _, []}, [{{:supervisor, _, :kernel_sup}, [_ | _]}]}]} =
+               :peer.call(peer, SystemInfo, :fetch_app_tree, [node(worker), :kernel])
+    end
+
+    defp start_peer() do
+      {:ok, peer, _node} =
+        :peer.start(%{
+          name: :peer.random_name(),
+          connection: :standard_io,
+          args: [~c"+S", ~c"2", ~c"-pa" | :code.get_path()]
+        })
+
+      on_exit(fn -> :peer.stop(peer) end)
+      {:ok, _} = :peer.call(peer, Application, :ensure_all_started, [:elixir])
+      peer
+    end
   end
 
   describe "fetch_memory_allocators" do
