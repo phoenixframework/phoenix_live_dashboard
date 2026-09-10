94d47da62d2ceacbe86a80e9d3a4868dcf469296
Fredrik Teschke <f@ftes.de>
2026-09-10 16:14:48 +0200

    Simplify remote process link regression test

--- test/phoenix/live_dashboard/system_info_remote_links_test.exs
+++ test/phoenix/live_dashboard/system_info_remote_links_test.exs
@@ -2,27 +2,20 @@ defmodule Phoenix.LiveDashboard.SystemInfoRemoteLinksTest do
   use ExUnit.Case, async: true
 
   alias Phoenix.LiveDashboard.SystemInfo
-  alias Phoenix.LiveDashboardTest.LinkedWorker
 
-  test "application trees skip remote links and retain local linked processes" do
+  test "application trees tolerate links to remote processes" do
     peer = start_peer()
     remote_peer = start_peer()
     remote_pid = :peer.call(remote_peer, :erlang, :spawn, [:timer, :sleep, [:infinity]])
 
-    child = %{id: LinkedWorker, start: {LinkedWorker, :start_link, [remote_pid]}}
+    child = %{id: :linked_worker, start: {Agent, :start_link, [:erlang, :link, [remote_pid]]}}
     {:ok, worker} = :peer.call(peer, Supervisor, :start_child, [:kernel_safe_sup, child])
-    local_pid = :peer.call(peer, Agent, :get, [worker, &Function.identity/1])
 
     assert {:links, links} = :peer.call(peer, Process, :info, [worker, :links])
     assert remote_pid in links
-    assert local_pid in links
 
-    tree = :peer.call(peer, SystemInfo, :app_tree_callback, [:kernel])
-    pids = tree_pids(tree)
-
-    assert worker in pids
-    assert local_pid in pids
-    refute remote_pid in pids
+    assert {{:master, _, []}, [_ | _]} =
+             :peer.call(peer, SystemInfo, :app_tree_callback, [:kernel])
   end
 
   defp start_peer do
@@ -36,9 +29,5 @@ defmodule Phoenix.LiveDashboard.SystemInfoRemoteLinksTest do
     on_exit(fn -> :peer.stop(peer) end)
     {:ok, _} = :peer.call(peer, Application, :ensure_all_started, [:elixir])
     peer
-  end
-
-  defp tree_pids({{_type, pid, _label}, children}) do
-    [pid | Enum.flat_map(children, &tree_pids/1)]
   end
 end
