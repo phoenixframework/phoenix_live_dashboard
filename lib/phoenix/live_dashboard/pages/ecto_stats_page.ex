d427eb3d9d5e3464c01b0c7c75127882c0e05eb4
George Guimarães <george.guimaraes@gmail.com>
2026-09-16 15:48:26 -0300

    fix: Silence optional Postgrex interval warning

--- lib/phoenix/live_dashboard/pages/ecto_stats_page.ex
+++ lib/phoenix/live_dashboard/pages/ecto_stats_page.ex
@@ -3,7 +3,8 @@ defmodule Phoenix.LiveDashboard.EctoStatsPage do
   use Phoenix.LiveDashboard.PageBuilder
   import Phoenix.LiveDashboard.Helpers
 
-  @compile {:no_warn_undefined, [Decimal, Duration, EctoPSQLExtras, {Ecto.Repo, :all_running, 0}]}
+  @compile {:no_warn_undefined,
+            [Decimal, Duration, Postgrex.Interval, EctoPSQLExtras, {Ecto.Repo, :all_running, 0}]}
   @disabled_link "https://hexdocs.pm/phoenix_live_dashboard/ecto_stats.html"
   @page_title "Ecto Stats"
 
